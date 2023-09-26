bbf35cb1ccc32e0df18de49d29cde5598289da36
Muhammad Alfian <me.alfians29@gmail.com>
2023-09-26 16:42:23 +0700

    update v4.0
    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,8 +1,75 @@
-import React from 'react';
-import './blog.css';
+import React from 'react'
+import './blog.css'
+
+import { BsArrowRightShort } from 'react-icons/bs'
+
+// import images
+import img from '../../Assets/image (2).png'
+import img2 from '../../Assets/image (3).png'
+import img3 from '../../Assets/image (9).jpg'
+import img4 from '../../Assets/image (10).png'
+
+// array method to display all the destination using (map)
+const Posts = [
+  {
+    id: 1,
+    postImage: img,
+    title: 'lorem ipsum dolor',
+    desc: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+  },
+  {
+    id: 2,
+    postImage: img2,
+    title: 'lorem ipsum dolor',
+    desc: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+  },
+  {
+    id: 3,
+    postImage: img3,
+    title: 'lorem ipsum dolor',
+    desc: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+  },
+  {
+    id: 4,
+    postImage: img4,
+    title: 'lorem ipsum dolor',
+    desc: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+  },
+]
 
 const Blog = () => {
-  return <div>Blog</div>;
-};
+  return (
+    <section className='blog container section'>
+      <div className='secContainer'>
+        <div className='secIntro'>
+          <h2 className='secTitle'>Our Best Blog?</h2>
+          <p>An insight to the incredible experience in the world!</p>
+        </div>
 
-export default Blog;
+        <div className='mainContainer grid'>
+          {Posts.map(({ id, postImage, title, desc }) => {
+            return (
+              <div className='singlePost grid'>
+                <div className='imgDiv'>
+                  <img src={postImage} alt={title} />
+                </div>
+
+                <div className='postDetails'>
+                  <h3>{title}</h3>
+                  <p>{desc}</p>
+                </div>
+
+                <a href='' className='flex'>
+                  Read More
+                  <BsArrowRightShort className='icon' />
+                </a>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Blog
